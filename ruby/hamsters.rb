5464932086d25b3b3c6120e711ebe61f59c986ed
Jessica Forchione <jsforchione@gmail.com>
2016-07-14 13:00:34 -0700

    complete hamster 4.3

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,18 +1,23 @@
-puts "What is the hamster's name?"
-name = gets.chomp
-puts "What is the volume level of your hamster? (1-10)"
-volume = gets.chomp
-puts "What is the hamster's fur color?"
-color = gets.chomp
-puts "Is the hamster a good candidate for adoption? (y/n)"
-adoption = gets.chomp
-puts "What is your hamster's estimated age?"
-age = gets.chomp
+def hamster
+  puts "What is the hamster's name?"
+  name = gets.chomp
+  puts "What is the volume level of your hamster? (1-10)"
+  volume = gets.chomp.to_i
+  puts "What is the hamster's fur color?"
+  color = gets.chomp
+  puts "Is the hamster a good candidate for adoption? (yes/no)"
+  adoption = gets.chomp
+  puts "What is your hamster's estimated age?"
+  age = gets.chomp
 
-def age(hamster)
-  if age == nil
-    age = nil
+  if age.empty?
+    age = "nil"
+  else age = gets.chomp.to_i
   end
-end
 
-puts "#{age}"
+  puts "Hamster's name: #{name}"
+  puts "Volume level: #{volume}"
+  puts "Hamster's fur color: #{color}"
+  puts "Good candidate for adoption: #{adoption}"
+  puts "Hamster's estimated age: #{age}"
+end
