10fdd469e5f732818664c821f7e21dbe07802477
Jessica Forchione <jsforchione@gmail.com>
2016-07-18 15:03:33 -0700

    completed method suspects

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,46 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+=> “InVeStIgAtIoN”
+"HeLlO".swapcase!
+=> "hElLo"
 
-# "zom".<???>
-# => “zoom”
+"zom".insert(2, "o")
+=> “zoom”
 
-# "enhance".<???>
-# => "    enhance    "
+"enhance".squeeze
+=> "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+"Stop! You’re under arrest!".upcase
+=> "STOP! YOU’RE UNDER ARREST!"
+"I'm under arrest!".upcase!
+=> "I'M UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
+"the usual".insert(9, " suspects")
+=> "the usual suspects"
 
-# " suspects".<???>
-# => "the usual suspects"
+" suspects".insert(0, "the usual")
+=> "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+"The case of the disappearing last letter".chop
+=> "The case of the disappearing last lette"
+"abcdefg".chop!
+=> "abcdef"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+"The mystery of the missing first letter".delete("T")
+=> "he mystery of the missing first letter"
+"nother way to delete, but modifies the original."
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+"Elementary,    my   dear        Watson!".squeeze(" ")
+=> "Elementary, my dear Watson!"
+"Squeezes     together,   cool!".squeeze(" ")
+=> "Squeezes together, cool!"
 
-# "z".<???>
-# => 122 
+"z".bytes
+=> 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# since "z" is a string it holds an arbitrary sequence of bytes
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+"How many times does the letter 'a' appear in this string?".count("a")
+=> 4
