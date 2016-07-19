6f3af1fc54633fe139dedaf8056d1521d93ff997
Jessica Forchione <jsforchione@gmail.com>
2016-07-19 12:24:18 -0700

    complete qs1-4 release 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -21,4 +21,47 @@ modified_flavors = ice_cream_flavors.map! do |flavor|
   p flavor.upcase
 end
 
+#Question 1
+numbers = { one: 1, two: 2, three: 3, four: 4} 
+letters =["a", "b", "c", "d"]
+
+numbers.delete_if { |number, integer| integer <= 1 }
+p numbers
+
+letters.delete_if {|letter| letter != "b"}
+p letters 
+
+#Question 2
+numbers = { one: 1, two: 2, three: 3, four: 4} 
+letters =["a", "b", "c", "d"]
+
+numbers.delete(:one)
+p numbers
+
+letters.delete_at(0)
+p letters
+
+#Question 3
+numbers = { one: 1, two: 2, three: 3, four: 4} 
+letters =["a", "b", "c", "d"]
+
+numbers.keep_if {|number, integer| integer <=2} 
+p numbers 
+
+modified_letters.map! { |letter| letter + "!" if letter == "b" }
+p modified_letters
+
+
+#Question 4
+numbers = { one: 1, two: 2, three: 3, four: 4} 
+letters =["a", "b", "c", "d"]
+
+numbers.reject! {|number, integer| integer <= (numbers.length + 3)} 
+p numbers 
+
+i = 0 
+while i < letters.length
+  letters.pop
+end
+p letters
 
